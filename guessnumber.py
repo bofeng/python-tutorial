0a7af3cfbdb566eec5af1d74d09ba16ff6230f4d
fengbo <vonbo@me.com>
2014-12-09 12:04:44 -0500

    add comment in guessnumber.py

--- guessnumber.py
+++ guessnumber.py
@@ -1,8 +1,14 @@
-import random
-number = random.randint(1, 20)
+from random import randint
+
+# Generate a random integer
+number = randint(1, 20)
 
 win = False
 hint = "What number I am thinking?"
+
+# Give user 3 chances to guess
+# use "break" to jump out a loop, if user guessed the right answer
+
 for i in range(3):
     guess = raw_input(hint)
     guess = int(guess)
@@ -16,5 +22,4 @@ for i in range(3):
         break
 
 if not win:
-    print "You Lose !!! The answer is "
-    print number
+    print "You Lose !!! The answer is ", number
